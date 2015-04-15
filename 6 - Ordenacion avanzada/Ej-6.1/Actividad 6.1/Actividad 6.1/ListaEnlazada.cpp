007e4f973c4a5d1da0c4caf5b1c7b2efa466fd4b
Sean <SeanNWJust@gmail.com>
2015-04-15 10:17:55 +0200

    Excervise 6.2 finished

---
+++ 6 - Ordenacion avanzada/Ej-6.1/Actividad 6.1/Actividad 6.1/ListaEnlazada.cpp
@@ -0,0 +1,265 @@
+#include "ListaEnlazada.h"
+#include "iostream"
+#include "cstdlib" // Para qsort
+#include "cassert"
+
+using namespace std;
+
+Nodo * ListaEnlazada::getNodo(int posicion) {
+	assert(posicion >= 0 && posicion <= n - 1); // Implica también que la lista no está vacía
+
+	Nodo *resultado = NULL; // Aquí guardaremos el puntero al nodo resultado 
+
+	// Si intentamos acceder al último nodo, lo hacemos directamente desde el primero, 
+	// para hacer que el acceso al último sea O(1)
+	if (posicion == n - 1) resultado = lista->getAnteriorNodo();
+
+	// En caso contrario, recorremos la lista hasta la posición correcta
+	else {
+		resultado = lista; // Hacemos que el resultado apunte al primer nodo
+		for (int i = 0; i<posicion; i++) resultado = resultado->getSiguienteNodo();
+	}
+
+	return (resultado);
+}
+
+void ListaEnlazada::vaciarLista() {
+	// Eliminamos el primer elemento de la lista. Esta operación la repetimos n veces
+	// Eliminar el primer elemento de la lista es O(1)
+	for (int i = 0; i<n; i++) eliminar(0);
+}
+
+
+ListaEnlazada::ListaEnlazada() {
+	lista = NULL; // No hay que reservar memoria ya que la lista se crea vacía en un principio
+	n = 0;
+}
+
+ListaEnlazada::ListaEnlazada(ListaEnlazada &loAsignado) {
+	// Creamos una lista vacía y luego copiamos loAsignado en ella
+	lista = NULL;
+	n = 0;
+	*this = loAsignado; // Llamamos al operador de asignación. Es O(n)
+}
+
+Elemento ListaEnlazada::getValor(int posicion) {
+	assert(posicion >= 0 && posicion <= n - 1); // Implica también que la lista no está vacía
+	// Obtenemos el nodo que contiene el elemento
+	Nodo *nodoQueContieneResultado = getNodo(posicion);
+	return (nodoQueContieneResultado->getElemento());
+}
+
+void ListaEnlazada::setValor(int posicion, Elemento nuevoValor) {
+	assert(posicion >= 0 && posicion <= n - 1); // Implica también que la lista no está vacía
+	getNodo(posicion)->setElemento(nuevoValor);
+}
+
+int ListaEnlazada::getN() {
+	return (n);
+}
+
+void ListaEnlazada::insertar(int posicion, Elemento nuevoValor) {
+	assert(posicion >= 0 && posicion <= n);
+
+	// Creamos el nuevo nodo con el nuevo valor
+	Nodo *nuevoNodo = new Nodo(nuevoValor);
+
+	// Si la lista estaba vacia...
+	if (n == 0) {
+		// Si sólo hay un nodo, ese nodo es el primero y el final de la lista, por lo tanto sus punteros
+		// anterior y siguiente apuntan a sí mismo
+		nuevoNodo->setAnteriorNodo(nuevoNodo);
+		nuevoNodo->setSiguienteNodo(nuevoNodo);
+	}
+
+	// Si la lista no está vacía
+	else {
+		// Primero obtenemos el nodo que está a continuación del que queremos insertar
+		// Nota que, si queremos insertar al final, su siguiente es el primero
+		Nodo *nodoSiguiente;
+		if (posicion == n) nodoSiguiente = lista;
+		else nodoSiguiente = getNodo(posicion);
+
+		// Obtenemos el nodo anterior al que queremos insertar
+		Nodo *nodoAnterior = nodoSiguiente->getAnteriorNodo();
+
+		// Actualizamos los punteros del nuevo nodo
+		nuevoNodo->setAnteriorNodo(nodoAnterior);
+		nuevoNodo->setSiguienteNodo(nodoSiguiente);
+
+		// Actualizamos los punteros de los dos nodos que rodean al nuevo nodo
+		// El puntero siguiente del anterior y el puntero anterior del siguiente apuntarán ambos al nuevo nodo
+		nodoSiguiente->setAnteriorNodo(nuevoNodo);
+		nodoAnterior->setSiguienteNodo(nuevoNodo);
+	} // Fin else
+
+	// Incrementamos el numero de elementos
+	n++;
+
+	// Si estamos insertando al principio de la lista, hay que modificar el puntero a la lista
+	if (posicion == 0) lista = nuevoNodo;
+}
+
+void ListaEnlazada::eliminar(int posicion) {
+	assert(posicion >= 0 && posicion <= n - 1); // Ya implica que la lista no puede estar vacía
+
+	// Obtenemos el nodo que queremos eliminar
+	Nodo *nodoAEliminar = getNodo(posicion);
+
+	// Si la lista sólo tiene un elemento, actualizamos el atributo lista pues la lista se queda vacia
+	if (n == 1) lista = NULL;
+
+	// Si la lista tiene más de un elemento, hacemos el caso general
+	else {
+		// Obtenemos los dos nodos que le rodean
+		Nodo *nodoAnterior = nodoAEliminar->getAnteriorNodo();
+		Nodo *nodoSiguiente = nodoAEliminar->getSiguienteNodo();
+		// Modificamos los punteros de cada uno de los dos nodos que rodean al que queremos eliminar
+		nodoAnterior->setSiguienteNodo(nodoSiguiente);
+		nodoSiguiente->setAnteriorNodo(nodoAnterior);
+		// Si el nodo a eliminar era el 0, tenemos que actualizar el atributo lista
+		if (posicion == 0) lista = nodoSiguiente;
+	} // Fin else
+
+	// Liberamos la memoria del nodo
+	delete(nodoAEliminar);
+
+	// Actualizamos n
+	n--;
+
+}
+
+ListaEnlazada& ListaEnlazada::operator= (ListaEnlazada &loAsignado) {
+
+	// Primero eliminamos todos los elementos de la lista actual, pues ya no nos sirven
+	// Esto es importante pues hay que liberar la memoria que ocupaban los viejos nodos!
+	// Es O(n)
+	vaciarLista();
+
+
+	// Ahora volvemos a crear la lista, pero con los elementos de loAsignado. 
+	// Para ello, insertamos sucesivamente al final de la lista
+	// Este bucle es O(loAsignado.n) porque insertar al final de la lista es O(1)
+	for (int i = 0; i<loAsignado.n; i++) insertar(i, loAsignado.getValor(i));
+
+	// Devolvemos la referencia al resultado, que es la nueva lista actual
+	return (*this);
+}
+
+ListaEnlazada& ListaEnlazada::operator+ (ListaEnlazada &operador2) {
+
+	// Creamos la lista que será el resultado
+	// Tiene que ser dinámica (creada con new) para que no desaparezca una vez terminado este método
+	ListaEnlazada *resultado = new ListaEnlazada();
+
+	// Copiamos la lista actual en la nueva lista
+	*resultado = *this;
+
+	// Copiamos la segunda lista (operador2) al final de la nueva lista.
+	// No nos queda más remedio que ir insertando elemento a elemento
+	for (int i = 0; i<operador2.n; i++) resultado->insertar(n + i, operador2.getValor(i));
+
+	// Devolvemos el nuevo objeto
+	return (*resultado);
+}
+
+Elemento& ListaEnlazada::operator[] (int posicion) {
+	assert(posicion >= 0 && posicion <= n - 1); // Ya implica que la lista no puede estar vacía
+
+	// Es necesario guardar el retorno de getValor() en un objeto creado dinámicamente
+	// para que no se destruya automáticamente al finalizar el método
+	Elemento *resultado = new Elemento(getValor(posicion));
+
+	// Devolvemos el resultado
+	// OJO: tal como está, el resultado de este operador no podría ser un lvalue en una asignación, para ello el elemento devuelto debería
+	// ser exactamente el que está dentro del nodo, y no una copia del mismo. 
+	return (*resultado);
+}
+
+int ListaEnlazada::buscar(Elemento elementoABuscar) {
+	int posicion = 0; // Posición actual en donde buscamos
+	bool encontrado = false; // Nos indica si hemos encontrado o no el elemento
+
+	// Buscamos el elemento hasta que lo encontremos o hasta que lleguemos al final
+	while (!encontrado && posicion < n) {
+		if (getValor(posicion) == elementoABuscar) encontrado = true;
+		posicion++;
+	}
+
+	// Devolvemos la posición si lo hemos encontrado, o -1 en caso contrario
+	if (encontrado) return (posicion - 1);
+	else return (-1);
+}
+
+ListaEnlazada::~ListaEnlazada() {
+	vaciarLista();
+}
+
+void ListaEnlazada::ordenar() {
+	// Si la lista está vacía o tiene un solo nodo, ya está ordenada y no hay que hacer nada
+	if (n >= 2) ordenarPorMergeSort(*this);
+}
+
+
+void ListaEnlazada::ordenarPorMergeSort(ListaEnlazada &lista) {
+
+	// Sólo hacemos algo si la lista tiene al menor 2 nodos 
+	// Por tanto no hay llamada recursiva (caso trivial) cuando se tiene 1 nodo o menos
+	if (lista.getN() >= 2) {
+
+		// Listas que contendrán a cada una de las dos partes en que dividiremos la lista enlazada
+		ListaEnlazada sublista1, sublista2; 
+
+		// Los nodos de posición par los ponemos en la primera sublista, y los impares en la segunda
+		int n = lista.getN(); // Tamaño de la lista pasada como parametro
+		for (int i=0; i<n; i++) {
+			if (i%2 == 0) {
+				sublista1.insertar(sublista1.getN(), lista.getValor(0));
+				lista.eliminar(0);
+			}
+			else {
+				sublista2.insertar(sublista2.getN(), lista.getValor(0));
+				lista.eliminar(0);
+			}
+		}
+
+		// Hacemos la llamada recursiva para ordenar ambas sublistas por separado
+		ordenarPorMergeSort(sublista1);
+		ordenarPorMergeSort(sublista2);
+
+		// Combinamos ambas sublistas y metemos la combinación en el parámetro de la función
+		// Vamos cogiendo el menor elemento del principio de ambas sublistas, hasta que una de ellas quede vacía
+		// En ese momento ponemos la lista no vacía tal cual a continuación
+		while (sublista1.getN() > 0 && sublista2.getN() > 0) {
+
+			// Calculamos cuál es la sublista que en su primera posición contiene el menor elemento
+			ListaEnlazada *sublistaConMenor;
+			if (sublista1.getValor(0) <= sublista2.getValor(0)) sublistaConMenor = &sublista1;
+			else sublistaConMenor = &sublista2;
+
+			// Ahora sacamos ese menor elemento de la lista y lo metemos en el resultado
+			Elemento menor = sublistaConMenor->getValor(0);
+			sublistaConMenor->eliminar(0); // Tenemos que eliminarle para no gastar memoria adicional O(n)
+
+			// Insertamos el menor en el resultado
+			lista.insertar(lista.getN(), menor);
+
+		}
+
+		// Ahora una de las sublistas está vacía. Averiguamos cuál
+		ListaEnlazada *sublistaNoVacia;
+		if (sublista1.getN() == 0) sublistaNoVacia = &sublista2;
+		else sublistaNoVacia = &sublista1;
+
+		// Insertamos la no vacía a continuación del resultado 
+		// No podemos usar el operador de concatenar porque entonces estaríamos teniendo memoria adicional O(n)
+		while (sublistaNoVacia->getN() > 0) {
+			lista.insertar(lista.getN(), sublistaNoVacia->getValor(0));
+			sublistaNoVacia->eliminar(0); // Tenemos que ir eliminando a medida que vamos añadiendo en el otro lado, para que la memoria adicional no sea O(n)
+		}
+
+	} // Fin if (lista.getN() >= 2)
+} 
+
+
+
---
+++ 6 - Ordenacion avanzada/Ej-6.1/Actividad 6.1/Actividad 6.1/main.cpp
@@ -0,0 +1,29 @@
+#include "iostream"
+#include "ListaEnlazada.h"
+#include "cstdlib" // Para srand y rand
+#include "ctime" // Para time()
+#include "BibliotecaImpresionListasEnlazadas.h"
+
+using namespace std;
+
+
+void main () {
+
+	ListaEnlazada lista; // Lista con la que haremos las pruebas
+	int n; // Tamaño de la lista
+
+	srand(time(NULL));
+
+	cout << "Introduce el numero de elementos de la lista: ";
+	cin >> n;
+
+	// Llenamos la lista con numeros aleatorios entre 20 y 40 e imprimimos
+	for (int i=0; i<n; i++) lista.insertar(lista.getN(), Elemento((float)(rand()%21 + 20)));
+	cout << "Lista sin ordenar: " << lista << endl;
+
+	// Ordenamos e imprimimos
+	lista.ordenar();
+	cout << "Lista ordenada con MergeSort: " << lista << endl;
+}
+
+
